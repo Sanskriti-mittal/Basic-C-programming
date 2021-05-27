724941173a6590cbdbc6597d0176f60e7cddb0fc
Sanskriti-mittal <84907827+Sanskriti-mittal@users.noreply.github.com>
2021-05-27 16:09:53 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,13 @@
+/* Calculate multiply of two numbers*/
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the value of a");
+	scanf("%d", &a);
+	printf("Enter the value of b");
+	scanf("%d", &b);
+	c= a*b;
+	printf("The multiplication of a and b= %d", c);
+	return 0;
+}
