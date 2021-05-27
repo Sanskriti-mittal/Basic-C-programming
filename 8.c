ad9e425ee7473c3cda1d37a5b62e57b80de9173e
Sanskriti-mittal <84907827+Sanskriti-mittal@users.noreply.github.com>
2021-05-27 16:13:34 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,10 @@
+/* Calculate ASCII Value (contains 256 values) */ 
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Input any ASCII value");
+	scanf("%d", &a);
+	printf("char=%c", a);
+	return 0;
+}
