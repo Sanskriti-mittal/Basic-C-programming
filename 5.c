9fae4d83769e09e79aee60c4b7cded18ce06af19
Sanskriti-mittal <84907827+Sanskriti-mittal@users.noreply.github.com>
2021-05-27 16:10:48 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,14 @@
+/* Calculate the division of two numbers*/
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the value of a");
+	scanf("%d", &a);
+	printf("Enter the value of b");
+	scanf("%d", &b);
+	c= a/b;
+	printf("Division of a and b = %d",c);
+	return 0;
+
+}
