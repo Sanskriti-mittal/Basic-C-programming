18374ccfe046d946c3879e3f45feb73f7989b938
Sanskriti-mittal <84907827+Sanskriti-mittal@users.noreply.github.com>
2021-05-27 16:11:32 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,14 @@
+/* Calculate average of two numbers*/
+#include<stdio.h>
+int main ()
+{
+	float a,b,c;
+	printf("Enter the value of a");
+	scanf("%f", &a);
+	printf("Enter the value of b");
+	scanf("%f", &b);
+	c= (a+b)/2;
+	printf("Average of a and b = %f",c);
+	return 0;
+	
+}
