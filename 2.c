b35ce5944b8de55cb491dd534b58de0f6151f7ae
Sanskriti-mittal <84907827+Sanskriti-mittal@users.noreply.github.com>
2021-05-27 16:07:09 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,14 @@
+/*Calculate sum of two numbers*/
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the value of a");
+	scanf("%d", &a);
+	printf("Enter the value of b");
+	scanf("%d", &b);
+	c=a+b;
+	printf("Sum of a and b= %d", c);
+	return 0;
+	
+}
